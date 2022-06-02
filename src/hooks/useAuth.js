3cad8670792305edfd19da5018e0045ec3ed2b96
adote o toto <adoteototo@gmail.com>
2022-06-02 12:20:53 -0300

    9- criando useAuth e provider api
    provider api é a base url do servidor que é provido pelo axios

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import api from "../utils/api";
+
+export default function useAuth() {
+  async function register(user) {
+    try {
+      const data = await api.post("/users/register", user).then((response) => {
+        return response.data;
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return { register };
+}
