9d03f04ed3ae929160c16144fefb60310b5c2eae
adote o toto <adoteototo@gmail.com>
2022-06-02 11:17:43 -0300

    6- definindo form e componente de input

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,7 +1,53 @@
 import React from "react";
-
+import Input from "../form/Input";
 const Register = () => {
-  return <h1>Register</h1>;
+  function handleChange() {}
+  return (
+    <section>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleChange}
+        />
+
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Digite a sua confirmação de senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
+    </section>
+  );
 };
 
 export default Register;
