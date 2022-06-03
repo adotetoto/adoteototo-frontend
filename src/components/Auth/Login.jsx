369c7753af45fe3761a3b6b53e2920484348f75e
adote o toto <adoteototo@gmail.com>
2022-06-03 19:39:54 -0300

    18 - criando estrutura do login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Context } from "../../context/UserContext";
+import Input from "../form/Input";
+import styles from "../form/form.module.css";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <h1>Login</h1>;
+  function handleChange() {}
+  return (
+    <section className={styles.form_container}>
+      <h1>Login</h1>
+      <form>
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+
+        <input type="submit" value="entrar" />
+
+        <p>
+          Não tem conta ? <Link to="/register">Clique aqui.</Link>
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default Login;
