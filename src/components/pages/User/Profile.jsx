4ce8617b0b24e5a741e1cd2d6398acd3d0bbb91e
adote o toto <adoteototo@gmail.com>
2022-06-07 11:39:38 -0300

    24- definindo atualização de imagem

--- src/components/pages/User/Profile.jsx
+++ src/components/pages/User/Profile.jsx
@@ -7,6 +7,7 @@ import useFlashMessage from "../../../hooks/useFlashMessage";
 
 const Profile = () => {
   const [user, setUser] = useState({});
+  const [preview, setPreview] = useState();
   const [token] = useState(localStorage.getItem("token") || "");
   const { setFlashMessage } = useFlashMessage();
   useEffect(() => {
@@ -22,6 +23,7 @@ const Profile = () => {
   }, [token]);
 
   function onFileChange(e) {
+    setPreview(e.target.files[0]);
     setUser({ ...user, [e.target.name]: e.target.files[0] });
   }
   function handleChange(e) {
@@ -61,7 +63,16 @@ const Profile = () => {
     <section>
       <div className={styles.profile_header}>
         <h1>Perfil</h1>
-        <p>Preview de imagem</p>
+        {(user.image || preview) && (
+          <img
+            src={
+              preview
+                ? URL.createObjectURL(preview)
+                : `${process.env.REACT_APP_API}/images/users/${user.image}`
+            }
+            alt={user.name}
+          />
+        )}
       </div>
 
       <form onSubmit={handleSubmit} className={formStyles.form_container}>
