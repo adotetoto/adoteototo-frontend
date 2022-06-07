9fd67b09924ea5adcb07be8cc7eaa04e75b7108b
adote o toto <adoteototo@gmail.com>
2022-06-07 09:40:47 -0300

    21-definindo estrutura pagina profile

---
+++ src/components/pages/User/Profile.jsx
@@ -0,0 +1,74 @@
+import userEvent from "@testing-library/user-event";
+import React, { useState, useEffect } from "react";
+import formStyles from "../../form/form.module.css";
+import Input from "../../form/Input";
+import styles from "./Profile.module.css";
+const Profile = () => {
+  const [user, setUser] = useState({});
+  function onFileChange() {}
+  function handleChange() {}
+
+  return (
+    <section>
+      <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        <p>Preview de imagem</p>
+      </div>
+
+      <form className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu e-mail"
+          handleOnChange={handleChange}
+          value={user.email || ""}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ""}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ""}
+        />
+
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+          value={user.password || ""}
+        />
+
+        <Input
+          text="Confirmação da senha"
+          type="password"
+          name="password"
+          placeholder="Confirmação da senha"
+          handleOnChange={handleChange}
+        />
+
+        <input type="submit" value="Editar" />
+      </form>
+    </section>
+  );
+};
+
+export default Profile;
