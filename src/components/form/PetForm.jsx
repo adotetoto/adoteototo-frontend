6f94436a51b00f3e1af751fdc47627101beeab6a
adote o toto <adoteototo@gmail.com>
2022-06-08 10:58:49 -0300

    29- terminando o formulario de cadastro de pet
    - falta salvar no banco

--- src/components/form/PetForm.jsx
+++ src/components/form/PetForm.jsx
@@ -6,12 +6,45 @@ import Select from "./Select";
 const PetForm = ({ handleSubmit, petData, btnText }) => {
   const [pet, setPet] = useState(petData || {});
   const [preview, setPreview] = useState([]);
-  const coolors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"];
-  function onFileChange(e) {}
-  function handleChange(e) {}
-  function handleColor(e) {}
+  const colors = ["Branco", "Preto", "Cinza", "Caramelo", "Mesclado"];
+  function onFileChange(e) {
+    setPreview(Array.from(e.target.files));
+    setPet({ ...pet, images: [...e.target.files] });
+  }
+  function handleChange(e) {
+    setPet({ ...pet, [e.target.name]: e.target.value });
+  }
+  function handleColor(e) {
+    setPet({
+      ...pet,
+      color: e.target.options[e.target.selectedIndex].text,
+    });
+  }
+  function submit(e) {
+    e.preventDefault();
+    console.log(pet);
+    //handleSubmit(pet)
+  }
   return (
-    <form className={formStyles.form_container}>
+    <form onSubmit={submit} className={formStyles.form_container}>
+      <div className={formStyles.preview_pet_images}>
+        {preview.length > 0
+          ? preview.map((image, index) => (
+              <img
+                src={URL.createObjectURL(image)}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            ))
+          : pet.images &&
+            pet.image.map((image, index) => (
+              <img
+                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            ))}
+      </div>
       <Input
         text="Imagens do Pet"
         type="file"
@@ -47,7 +80,7 @@ const PetForm = ({ handleSubmit, petData, btnText }) => {
       <Select
         name="color"
         text="Selecione a cor"
-        options={coolors}
+        options={colors}
         handleOnChange={handleColor}
         value={pet.color || ""}
       />
