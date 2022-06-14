4aacac5bb0b15aa507c1ec31973247262601505c
adote o toto <adoteototo@gmail.com>
2022-06-14 20:11:24 -0300

    37 - finalizando update

--- src/components/pages/Pet/EditPet.jsx
+++ src/components/pages/Pet/EditPet.jsx
@@ -24,7 +24,34 @@ const EditPet = () => {
       });
   }, [token, id]);
 
-  async function updatePet(pet) {}
+  async function updatePet(pet) {
+    let msgType = "sucess";
+    const formData = new FormData();
+    await Object.keys(pet).forEach((key) => {
+      if (key === "images") {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append("images", pet[key][i]);
+        }
+      } else {
+        formData.append(key, pet[key]);
+      }
+    });
+    const data = await api
+      .patch(`pets/${pet._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          "Content-type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((err) => {
+        msgType = "error";
+        return err.response.data;
+      });
+    setFlashMessage(data.message, msgType);
+  }
   return (
     <section>
       <div className={styles.addpet_header}>
