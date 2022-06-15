f54fc8e8c10b92b179f547c62c662fac10eaa2b6
adote o toto <adoteototo@gmail.com>
2022-06-14 23:31:50 -0300

    39 - criando estrutura da home
    imprimindo os dados,porem nao formatados

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,7 +15,30 @@ function Home() {
     });
   }, []);
 
-  return <h1>Home</h1>;
+  return (
+    <section>
+      <div>
+        <h1>Adote um Pet</h1>
+        <p>Veja os detalhes de cada um conheça o tutor deles</p>
+      </div>
+
+      <div>
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div>
+              <p>Imagem do Pet</p>
+              <h3>{pet.name}</h3>
+              <p>
+                <span className="bold">Peso:</span> {pet.weight}kg
+                <Link to={`pet/${pet._id}`}>Mais detalhes</Link>
+              </p>
+            </div>
+          ))}
+
+        {pets.length === 0 && <p>Não há pets cadastrados</p>}
+      </div>
+    </section>
+  );
 }
 
 export default Home;
