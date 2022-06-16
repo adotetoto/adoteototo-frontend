35c8f0c34277e29757a0bbe4f66a7a986adcf91c
adote o toto <adoteototo@gmail.com>
2022-06-15 22:01:06 -0300

    42 - puxando os dados da api
    - puxando os detalhes da api do pet

--- src/components/pages/Pet/PetDetails.jsx
+++ src/components/pages/Pet/PetDetails.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import useFlashMessage from "../../../hooks/useFlashMessage";
+import api from "../../../utils/api";
 import styles from "./PetDetails.module.css";
 
 const PetDetails = () => {
-  return <h1>Pagina de Pet</h1>;
+  const [pet, setPet] = useState({});
+  const { id } = useParams();
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet);
+    });
+  }, [id]);
+  return <h1>{pet.name}</h1>;
 };
 
 export default PetDetails;
