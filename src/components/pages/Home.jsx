bd69ae31e4c456b24f42107a3fa0db98eefae64a
adote o toto <adoteototo@gmail.com>
2022-06-24 19:01:14 -0300

    50 - definindo estilo filtro home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -17,27 +17,79 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    if (filter) {
-      api.get(`/pets/filters/${filter}`).then((response) => {
-        setPets(response.data.pets);
-      });
-    } else {
-      api.get("/pets").then((response) => {
-        setPets(response.data.pets);
-      });
-    }
+    api.get(`/pets/filters/${filter}`).then((response) => {
+      setPets(response.data.pets);
+    });
   }, [filter]);
 
   return (
     <section>
       <div>
         <h1>Adote um Pet</h1>
-        <p>Veja os detalhes de cada um conheça o tutor deles</p>
+        {filter === "all" && (
+          <p>
+            <span>Todos</span> pets cadastrados no sistema.
+          </p>
+        )}
+        {filter === "macho" && (
+          <p>
+            Pets que são do sexo <span>macho</span>.
+          </p>
+        )}
+        {filter === "femea" && (
+          <p>
+            Pets que são do sexo <span>fêmea</span>.
+          </p>
+        )}
+        {filter === "portoalegre" && (
+          <p>
+            Pets que seus doadores estão em <span>Porto Alegre</span>.
+          </p>
+        )}
+        {filter === "viamao" && (
+          <p>
+            Pets que seus doadores estão em <span>Viamão</span>.
+          </p>
+        )}
+        {filter === "canoas" && (
+          <p>
+            Pets que seus doadores estão em <span>Canoas</span>.
+          </p>
+        )}
+        {filter === "gravatai" && (
+          <p>
+            Pets que seus doadores estão em <span>Gravatai</span>.
+          </p>
+        )}
+        {filter === "cachoeirinha" && (
+          <p>
+            Pets que seus doadores estão em <span>Cachoeirinha</span>.
+          </p>
+        )}
+        {filter === "esteio" && (
+          <p>
+            Pets que seus doadores estão em <span>Esteio</span>.
+          </p>
+        )}
+        {filter === "small" && (
+          <p>
+            Pets com o porte <span>pequeno</span>.
+          </p>
+        )}
+        {filter === "medium" && (
+          <p>
+            Pets com porte <span>médio</span>.
+          </p>
+        )}
+        {filter === "great" && (
+          <p>
+            Pets com porte <span>pequeno</span>.
+          </p>
+        )}
       </div>
 
       <div>
-        <h3>Filtro de sexo</h3>
-        <div>
+        <div className={styles.pet_filter_container}>
           <button onClick={() => setFilter("all")}>Todos</button>
           <button onClick={() => setFilter("macho")}>Macho</button>
           <button onClick={() => setFilter("femea")}>Femea</button>
@@ -49,7 +101,7 @@ function Home() {
             Cachoeirinha
           </button>
           <button onClick={() => setFilter("esteio")}>Esteio</button>
-          <button onClick={() => setFilter("viamao")}>Viamão</button>
+
           <button onClick={() => setFilter("small")}>Pequeno</button>
           <button onClick={() => setFilter("medium")}>Médio</button>
           <button onClick={() => setFilter("great")}>Grande</button>
@@ -77,7 +129,19 @@ function Home() {
             ))}
           {pets.length === 0 && (
             <p>
-              Não há pets cadastrados ou disponíveis para adoção no momento!
+              Não há pets cadastrados ou com o filtro selecionado
+              {filter === "all" && <span> Todos.</span>}
+              {filter === "macho" && <span> macho.</span>}
+              {filter === "femea" && <span> fêmea.</span>}
+              {filter === "portoalegre" && <span> Porto Alegre.</span>}
+              {filter === "viamao" && <span> Viamão.</span>}
+              {filter === "canoas" && <span> Canoas.</span>}
+              {filter === "esteio" && <span> Esteio.</span>}
+              {filter === "gravatai" && <span> Gravatai.</span>}
+              {filter === "cachoeirinha" && <span> Cachoeirinha.</span>}
+              {filter === "small" && <span> Pequeno.</span>}
+              {filter === "medium" && <span> Médio.</span>}
+              {filter === "great" && <span> Grande.</span>}
             </p>
           )}
         </div>
